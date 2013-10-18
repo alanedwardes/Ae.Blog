4d00682224a5b2732c75dc44e1c250ed3331875b
Alan Edwardes <alan@alanedwardes.com>
2013-10-18 01:28:46 +0100

    The redirect middleware now passes the trailing part of the URL, including query string to target URL.

--- blog/middleware.py
+++ blog/middleware.py
@@ -1,9 +1,7 @@
-from django import http
+from django.http import HttpResponseRedirect
 from blog.models import Redirect
 
 class RedirectMiddleware(object):
-	redirect = http.HttpResponseRedirect
-	
 	def deslash(self, url):
 		return url.strip('/')
 
@@ -11,15 +9,17 @@ class RedirectMiddleware(object):
 		if response.status_code != 404:
 			return response
 		
-		full_path = self.deslash(request.get_full_path())
+		request_directory = self.deslash(request.path)
+		request_fullpath = self.deslash(request.get_full_path())
 		
 		for redirect in Redirect.objects.all():
 			base = self.deslash(redirect.base)
-		
-			if base == full_path:
-				return self.redirect(redirect.redirect)
-		
-			if redirect.is_recursive and full_path[:len(base)] == base:
-				return self.redirect(redirect.redirect)
+			new_path = request_fullpath[len(base):]
+			
+			if base == request_directory:
+				return HttpResponseRedirect(redirect.redirect + new_path)
+			
+			if redirect.is_recursive and request_fullpath[:len(base)] == base:
+				return HttpResponseRedirect(redirect.redirect + new_path)
 		
 		return response
