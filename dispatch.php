dde7ab915883919cb83d65be9a2fc3638fb96fcc
Alan Edwardes <alan@alanedwardes.com>
2014-04-28 19:59:15 +0100

    Updated dispatcher to add the PHP session handler.

--- dispatch.php
+++ dispatch.php
@@ -9,6 +9,8 @@ require_once 'views.php';
 
 $router = new ae\Routing\CachedRouter(new ae\Caching\ApcCache(CACHE_KEY));
 
+$authenticator = new ae\Auth\ArrayAuthenticator($auth_credentials, new ae\Sessions\PHPSessionHandler('aeblog'));
+
 ae\Routing\RouteMap::map($router, [
 	['/estranged', 'AeFramework\Views\PermanentRedirectView', 'http://www.iamestranged.com/'],
 	['r^/estranged/(?P<path>.*)$', 'AeFramework\Views\PermanentRedirectView', 'http://www.iamestranged.com/%s'],
@@ -19,7 +21,7 @@ ae\Routing\RouteMap::map($router, [
 	['r^/portfolio/item/(?P<portfolio_id>.*)/$', 'PortfolioSingleView', 'templates/portfolio_single.html'],
 	['r^/portfolio/skill/(?P<skill_id>.*)/$', 'PortfolioSkillView', 'templates/portfolio.html'],
 	['/contact/', 'TemplateView', 'templates/contact.html'],
-	['r^/admin/', 'AeFramework\Extensions\Admin\AdminRouter', new ae\Auth\ArrayAuthenticator($auth_credentials)],
+	['r^/admin/', 'AeFramework\Extensions\Admin\AdminRouter', $authenticator],
 	[ae\Http\Code::NotFound, 'TemplateView', 'templates/404.html']
 ]);
 
