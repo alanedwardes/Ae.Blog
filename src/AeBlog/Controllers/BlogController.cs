8d154b58c5088575515f649f654a6babe72e6c42
Alan Edwardes <alan@alanedwardes.com>
2015-08-11 23:03:34 +0100

    Changes to BlogController.

--- src/AeBlog/Controllers/BlogController.cs
+++ src/AeBlog/Controllers/BlogController.cs
@@ -1,7 +1,11 @@
 ﻿using AeBlog.Data;
 using AeBlog.ViewModels;
 using Microsoft.AspNet.Mvc;
+using System;
+using System.Collections.Generic;
 using System.Linq;
+using System.Net;
+using System.Net.Http;
 using System.Threading;
 using System.Threading.Tasks;
 
@@ -11,18 +15,30 @@ namespace AeBlog.Controllers
     public class BlogController : Controller
     {
         private readonly IPostManager postManager;
+        private readonly ILastfmDataProvider lastfmDataProvider;
 
-        public BlogController(IPostManager postManager)
+        public BlogController(IPostManager postManager, ILastfmDataProvider lastfmDataProvider)
         {
             this.postManager = postManager;
+            this.lastfmDataProvider = lastfmDataProvider;
         }
 
         [Route("/")]
         public async Task<IActionResult> Home(CancellationToken ctx = default(CancellationToken))
         {
+            var albums = await lastfmDataProvider.GetTopAlbumsForUser("alanedwardes", "", "7day");
+
             var posts = await postManager.GetPosts(ctx);
 
-            return View(new HomeViewModel(posts));
+            return View(new HomeViewModel(posts, albums));
+        }
+
+        [Route("/archive/")]
+        public async Task<IActionResult> Archive(CancellationToken ctx = default(CancellationToken))
+        {
+            var posts = await postManager.GetPosts(ctx);
+
+            return View(new ArchiveViewModel(posts));
         }
 
         [Route("/posts/{slug}/")]
