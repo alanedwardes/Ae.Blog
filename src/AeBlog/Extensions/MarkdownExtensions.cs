31913affc056e60f0cedcb45e66d35581798c6a7
Alan Edwardes <alan@alanedwardes.com>
2020-02-09 13:32:14 +0000

    Adds support for strikethrough markdown, removes twitter feed.

---
+++ src/AeBlog/Extensions/MarkdownExtensions.cs
@@ -0,0 +1,26 @@
+﻿using AeBlog.Models;
+using CommonMark;
+using System;
+
+namespace AeBlog.Extensions
+{
+    public static class MarkdownExtensions
+    {
+        private static Lazy<CommonMarkSettings> converterSettings = new Lazy<CommonMarkSettings>(() =>
+        {
+            var settings = CommonMarkSettings.Default.Clone();
+            settings.AdditionalFeatures |= CommonMarkAdditionalFeatures.StrikethroughTilde;
+            return settings;
+        });
+
+        public static string GetMarkdown(this Post post)
+        {
+            if (post.IsSingle)
+            {
+                return CommonMarkConverter.Convert(post.ContentAll, converterSettings.Value);
+            }
+
+            return CommonMarkConverter.Convert(post.ContentSummary, converterSettings.Value);
+        }
+    }
+}
--- src/AeBlog/Models/Post.cs
+++ src/AeBlog/Models/Post.cs
@@ -2,7 +2,12 @@
 {
     public class Post : PostSummary
     {
+        private const string MoreMarker = "---";
+
         public string Content { get; set; }
+        public bool HasSummary => Content.Contains(MoreMarker);
+        public string ContentAll => Content.Replace(MoreMarker, string.Empty);
+        public string ContentSummary => Content.Split(MoreMarker)[0];
         public bool IsSingle { get; set; }
     }
 }
