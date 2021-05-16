eaac934980bd09f027390c2192730e982232b6d8
Alan Edwardes <alan@alanedwardes.com>
2021-05-16 20:41:12 +0100

    Adds model viewer.

---
+++ src/Ae.Blog/Controllers/ViewerController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+
+namespace Ae.Blog.Controllers
+{
+    public class ViewerController : Controller
+    {
+        public IActionResult Model(Guid id)
+        {
+            return View(new Uri("https://s.edward.es/" + id + ".glb"));
+        }
+    }
+}
