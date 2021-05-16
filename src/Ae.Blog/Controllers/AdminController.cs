3b8517de2fc365c02f43ce942b76000d8758ec3d
Alan Edwardes <alan@alanedwardes.com>
2021-05-16 21:12:15 +0100

    Makes URI explicitly relative.

--- src/Ae.Blog/Controllers/AdminController.cs
+++ src/Ae.Blog/Controllers/AdminController.cs
@@ -120,7 +120,7 @@ namespace Ae.Blog.Controllers
             };
             freezerConfiguration.AdditionalResources.Add(new Uri("sitemap.xml", UriKind.Relative));
             freezerConfiguration.AdditionalResources.Add(new Uri("lib/highlight/atom-one-dark.min.css", UriKind.Relative));
-            freezerConfiguration.AdditionalResources.Add(new Uri("viewer/"));
+            freezerConfiguration.AdditionalResources.Add(new Uri("viewer/", UriKind.Relative));
 
             await freezer.Freeze(freezerConfiguration, CancellationToken.None);
             return Redirect(Url.Action(nameof(Index)));
